a0f3b1f5b513464f6150c1f3eb5f231aae0f9659
shettydhanush13 <dhanush@piktorlabs.com>
2021-05-21 11:19:02 +0530

    feat: basic express backend setup

---
+++ backend/src/index.js
@@ -0,0 +1,11 @@
+const app = require('./app');
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, (err) => {
+  if (err) {
+    console.error(`ERROR: ${err.message}`);
+  } else {
+    console.log(`Listening on port ${port}`);
+  }
+});
