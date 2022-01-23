b968e767dbee002c1ed9333f4660f12882a4a45a
Dhanush Shetty <dhanush.shetty@cimpress.com>
2022-01-23 13:32:36 +0530

    feat

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const app = require('./app');
 const { errorHandler, logger } = require('./core');
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8000;
 
 app.listen(port, (err) => {
   if (err) {
